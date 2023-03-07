1791fe87dcdded4322dda2ee007a52649c254cf4
okuwa <k-okuwa@netraise.jp>
2023-03-07 10:27:21 +0900

    redisのポート番号の変更

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://localhost:8081' }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://localhost:6379' }
+  config.redis = { url: 'redis://localhost:8081' }
 end
