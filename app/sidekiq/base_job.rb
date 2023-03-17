69d5315be225e2ca13210a35bd124dce0ed6aa7d
okuwa <k-okuwa@netraise.jp>
2023-03-17 16:18:36 +0900

    編集

--- app/sidekiq/base_job.rb
+++ app/sidekiq/base_job.rb
@@ -66,68 +66,55 @@ class BaseJob
     companies = Array.new
     # ループ処理
     100.times do
-      begin
-        # メールシステム一覧のlinkを取得（aタグ）
-        lists = session.all(:xpath, "//h4[@class='searches__result__list__header__title']/a")
-        # 取得したメールシステム一覧のaタグ要素をループ処理してパス（セレクター）を取得
-        list_paths = lists.map{|element| element.path}
-        # パスをループ処理する
-        list_paths.each.with_index(1) do |list_path, i|
-          # break if i >= 4
-          # xpathを元に要素の取得
-          list_link = session.find(:xpath, list_path)
-          # 要素をホバー
-          list_link.hover
-          # 要素をクリック
-          list_link.click
-          sleep(rand(1..3))
-          # html情報をnokogiriで処理
-          doc = Nokogiri::HTML.parse(session.html)
-          # 会社名と記載したtextがあるdtタグの後ろのddタグのテキスト（⚠︎本当はif分を一つずつ記載するべき）
-          # xpathを元に要素のテキストを取得
-          name = doc.xpath("//h1[@class='node__header__text__title']").text.squish
-          address = doc.xpath("//dt[contains(text(),'住所')]/following-sibling::dd").text.squish
-          url = doc.xpath("//a[text()='会社サイト']/@href").text.squish
-          puts name #デバック用
-          # ハッシュを作成する
-          hash = {name: name, address: address, url: url}
-          # 結果格納用配列にハッシュを追加する
-          companies.push(hash)
-          # 元いたタブのhtml情報をnokogiriで処理
-          session.go_back
-          sleep(1)
-        end
+      # メールシステム一覧のlinkを取得（aタグ）
+      lists = session.all(:xpath, "//h4[@class='searches__result__list__header__title']/a")
+      # 取得したメールシステム一覧のaタグ要素をループ処理してパス（セレクター）を取得
+      list_paths = lists.map{|element| element.path}
+      # パスをループ処理する
+      list_paths.each.with_index(1) do |list_path, i|
+        # break if i >= 4
+        # xpathを元に要素の取得
+        list_link = session.find(:xpath, list_path)
+        # 要素をホバー
+        list_link.hover
+        # 要素をクリック
+        list_link.click
+        sleep(rand(1..3))
         # html情報をnokogiriで処理
         doc = Nokogiri::HTML.parse(session.html)
-        # xpathを元に要素の取得
-        pages = doc.xpath("//em[@class='current']/following-sibling::a[1]")
-        # loop処理を停止するための処理
-        # ページ遷移のページリンクがある場合、次のページへのページリンクをクリック。
-        if pages.present?
-          session.find("//em[@class='current']/following-sibling::a[1]").click
-          sleep(1)
-        else
-          break
-        end
-      rescue => exception
-        puts exception
-        puts "処理に失敗したためスクリプトを終了します"
+        # 会社名と記載したtextがあるdtタグの後ろのddタグのテキスト（⚠︎本当はif分を一つずつ記載するべき）
+        # xpathを元に要素のテキストを取得
+        name = doc.xpath("//h1[@class='node__header__text__title']").text.squish
+        address = doc.xpath("//dt[contains(text(),'住所')]/following-sibling::dd").text.squish
+        url = doc.xpath("//a[text()='会社サイト']/@href").text.squish
+        puts name #デバック用
+        # ハッシュを作成する
+        hash = {name: name, address: address, url: url}
+        # 結果格納用配列にハッシュを追加する
+        companies.push(hash)
+        # 元いたタブのhtml情報をnokogiriで処理
+        session.go_back
+        sleep(1)
+      end
+      # html情報をnokogiriで処理
+      doc = Nokogiri::HTML.parse(session.html)
+      # xpathを元に要素の取得
+      pages = doc.xpath("//em[@class='current']/following-sibling::a[1]")
+      # loop処理を停止するための処理
+      # ページ遷移のページリンクがある場合、次のページへのページリンクをクリック。
+      if pages.present?
+        session.find("//em[@class='current']/following-sibling::a[1]").click
+        sleep(1)
+      else
         break
-      ensure
-        # 必ず処理する内容
       end
     end
     puts "ループ終了"
-    begin
       # ループ終了時の現在のブラウザのURLを表示。
       puts session.current_url
       # ブラウザを閉じる
       session.driver.quit
       sleep(1)
-    rescue => exception
-      puts exception
-      puts "ブラウザの操作に失敗しました"
-    end
     # スクレイピングした情報を元にCSVのデータを作成
     # Windowsで開くことを想定しているのでUTF-8 BOM付きのCSVファイルを作成する
     bom ="\xEF\xBB\xBF" # bomを作成
